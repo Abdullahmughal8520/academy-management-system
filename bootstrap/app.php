1162111d92d2f41d3897ce9d985e86904ad44347
Abdullah <abdullah.mughal8520@gmail.com>
2026-09-10 04:06:12 -0700

    Fix root route and HTTPS trust

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,3 @@
-
 <?php
 
 use Illuminate\Foundation\Application;
@@ -15,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
     'admin' => AdminMiddleware::class,
     'teacher' => TeacherMiddleware::class,
